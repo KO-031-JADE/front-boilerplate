709bc27d6e31cf52dbabec30f0295fc2743981cb
DESKTOP-QC78LBU\4M-FE <gasina85@gmail.com>
2024-10-28 18:27:18 +0900

    [2024_하반기_공모전] 이미지추가,  투표하기, 점수확인, 카테고리 코드수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,8 @@ document.addEventListener("DOMContentLoaded", function () {
   handleReceptionButtonFloating(); // 접수하러 가기 버튼 애니메이션
   handlePopup(); // 팝업
   toggleNoticeTitles(); // 공지사항 토글
-  handleTabs(); // 투표하기 탭 애니메이션
-  handleCateTabs(); // 카테고리 탭 애니메이션
+  handleTabs(); // 투표하기, 점수확인 탭
+  handleCateTabs(); // 카테고리 탭
 });
 
 // 헤더의 애니메이션
@@ -16,14 +16,13 @@ function handleHeaderAnimation() {
   }
 
   const headerHeight = header.offsetHeight;
-  window.onscroll = () => {
-    const windowTop = window.scrollY;
-    if (windowTop >= headerHeight) {
+  window.addEventListener("scroll", () => {
+    if (window.scrollY >= headerHeight) {
       header.classList.add("drop");
     } else {
       header.classList.remove("drop");
     }
-  };
+  });
 }
 
 // 팝업
@@ -43,11 +42,7 @@ function handlePopup() {
   closeBtn.addEventListener("click", () => popup.style.display = "none");
 
   // 페이지 로드 시 쿠키 검사 후 팝업 표시 결정
-  if (getCookie("hideLegoPopup") !== "true") {
-    popup.style.display = "block";
-  } else {
-    popup.style.display = "none";
-  }
+  popup.style.display = getCookie("hideLegoPopup") !== "true" ? "block" : "none";
 }
 
 // 쿠키 설정 후 팝업 숨김 처리
@@ -58,99 +53,52 @@ function setCookieAndHidePopup(popup) {
 
 // 쿠키를 설정하는 함수
 function setCookie(name, value, days) {
-  let expires = "";
-  if (days) {
-    const date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    expires = "; expires=" + date.toUTCString();
-  }
-  document.cookie = `${name}=${value}${expires}; path=/`;
+  const date = new Date();
+  date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+  document.cookie = `${name}=${value}; expires=${date.toUTCString()}; path=/`;
 }
 
 // 쿠키를 가져오는 함수
 function getCookie(name) {
   const nameEQ = `${name}=`;
-  const ca = document.cookie.split(';');
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i].trim();
-    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length);
-  }
-  return null;
+  return document.cookie.split(';').map(c => c.trim()).find(c => c.indexOf(nameEQ) === 0)?.substring(nameEQ.length) || null;
 }
 
 // 공지사항 타이틀 클릭 시 토글
 function toggleNoticeTitles() {
-  const noticeTitles = document.querySelectorAll('.notice-lists_tit');
-  noticeTitles.forEach(title => {
+  document.querySelectorAll('.notice-lists_tit').forEach(title => {
     title.addEventListener('click', () => title.classList.toggle('open'));
   });
 }
 
-
-// 투표하기 탭 애니메이션
+// 투표하기, 점수확인 탭
 function handleTabs() {
   const tabs = document.querySelectorAll('.tabs-wrap .tab');
-  const indicator = document.querySelector('.tabs-wrap .indicator');
 
-  tabs.forEach((tab, index) => tab.addEventListener('click', () => {
+  tabs.forEach((tab) => tab.addEventListener('click', () => {
     document.querySelector('.tabs-wrap .tab.active').classList.remove('active');
     tab.classList.add('active');
-    const tabWidth = tab.offsetWidth;
-    const tabLeft = tab.offsetLeft;
-    indicator.style.transform = `translateX(${tabLeft}px)`;
-    indicator.style.width = `${tabWidth}px`;
   }));
-
-  // 초기 active 상태 설정
-  const activeTab = document.querySelector('.tabs-wrap .tab.active');
-  if (activeTab) {
-    const tabWidth = activeTab.offsetWidth;
-    const tabLeft = activeTab.offsetLeft;
-    const indicator = document.querySelector('.tabs-wrap .indicator');
-    indicator.style.transform = `translateX(${tabLeft}px)`;
-    indicator.style.width = `${tabWidth}px`;
-  }
 }
 
-// 카테고리 탭 애니메이션
+// 카테고리 탭
 function handleCateTabs() {
   const tabs = document.querySelectorAll('.cate-wrap .tab');
-  const indicator = document.querySelector('.cate-wrap .indicator');
 
-  tabs.forEach((tab, index) => tab.addEventListener('click', () => {
+  tabs.forEach((tab) => tab.addEventListener('click', () => {
     document.querySelector('.cate-wrap .tab.active').classList.remove('active');
     tab.classList.add('active');
-    const tabWidth = tab.offsetWidth;
-    const tabLeft = tab.offsetLeft;
-    indicator.style.transform = `translateX(${tabLeft}px)`;
-    indicator.style.width = `${tabWidth}px`;
   }));
-
-  // 초기 active 상태 설정
-  const activeTab = document.querySelector('.cate-wrap .tab.active');
-  if (activeTab) {
-    const tabWidth = activeTab.offsetWidth;
-    const tabLeft = activeTab.offsetLeft;
-    const indicator = document.querySelector('.cate-wrap .indicator');
-    indicator.style.transform = `translateX(${tabLeft}px)`;
-    indicator.style.width = `${tabWidth}px`;
-  }
 }
 
 // 접수하러 가기 플로팅 애니메이션
 function handleReceptionButtonFloating() {
   // 기존 css에서 플로팅 배너 위치(top)값을 가져와 저장한다.	
-  // var floatPosition = parseInt($("#floatButton").css('top'));	
   var floatPosition = 712;	
-  // 250px 이런식으로 가져오므로 여기서 숫자만 가져온다. parseInt( 값 ); 	
   $(window).scroll(function() {		
     // 현재 스크롤 위치를 가져온다.		
     var scrollTop = $(window).scrollTop();		
-    var newPosition = scrollTop + floatPosition + "px"; 		
-    /* 애니메이션 없이 바로 따라감		 $("#floatMenu").css('top', newPosition);		 */ 
-    $("#floatButton").stop().animate({			"top" : newPosition		}, 500); 	
+    var newPosition = scrollTop + floatPosition + "px";		
+    $("#floatButton").stop().animate({ "top": newPosition }, 500);	
   }).scroll();
 }
-
-
-
